aaac54b993823d3dc430106b212ce57766962cac
Lebaohuy <77989902+Lebaohuy@users.noreply.github.com>
2021-08-16 22:33:13 +0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,9 @@
+ <?php
+$bs_nameweb = "SHOP VIP VC";
+$bs_main_url = "https://code by duong st .com/";
+//=== Không cần chỉnh phần dưới , nếu lỗi inbox zalo 0868158117  setup giúp hehe
+$bs_anticode = "<style>img[alt='www.000webhost.com']{display:none;}</style>";
+$bs_email = "";
+$bs_alert = "Vui lòng chọn game bạn cần nạp tiền.";
+$bs_title = "Trang nhận quà Freefire lớn nhất Việt Nam";
+$bs_desciption = "Trang nhận quà Freefire online lớn nhất Việt Nam!";
